66a824a0b2c84b44c056a0294bbf29fa3a71f47a
CordlessCoder <>
2023-03-21 17:52:17 +0000

    improved LSP support

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -46,6 +46,35 @@ return {
 			cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
 		end,
 	},
+
+	{
+		"lukas-reineke/indent-blankline.nvim",
+		lazy = true,
+		event = "BufEnter",
+		config = function()
+			require("indent_blankline").setup({
+
+				vim.cmd([[highlight IndentBlanklineIndent1 guifg=#E06C75 gui=nocombine]]),
+				vim.cmd([[highlight IndentBlanklineIndent2 guifg=#E5C07B gui=nocombine]]),
+				vim.cmd([[highlight IndentBlanklineIndent3 guifg=#98C379 gui=nocombine]]),
+				vim.cmd([[highlight IndentBlanklineIndent4 guifg=#56B6C2 gui=nocombine]]),
+				vim.cmd([[highlight IndentBlanklineIndent5 guifg=#61AFEF gui=nocombine]]),
+				vim.cmd([[highlight IndentBlanklineIndent6 guifg=#C678DD gui=nocombine]]),
+				-- for example, context is off by default, use this to turn it on
+				show_current_context = true,
+				char_highlight_list = {
+					"IndentBlanklineIndent1",
+					"IndentBlanklineIndent2",
+					"IndentBlanklineIndent3",
+					"IndentBlanklineIndent4",
+					"IndentBlanklineIndent5",
+					"IndentBlanklineIndent6",
+				},
+			})
+		end,
+	},
+	{ "nvim-treesitter/nvim-treesitter-context", lazy = true, event = "BufEnter" },
+
 	{
 		"wakatime/vim-wakatime",
 		lazy = true,
