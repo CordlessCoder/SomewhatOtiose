a0cfcda12f3142625c75d042488bc0d9bd28be2b
CordlessCoder <42666308+CordlessCoder@users.noreply.github.com>
2024-04-21 12:32:54 +0100

    Fix bootstrapping

--- init.lua
+++ init.lua
@@ -4,7 +4,7 @@ local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
 	vim.fn.system({
 		"git",
-		"tlone",
+		"clone",
 		"--filter=blob:none",
 		"https://github.com/folke/lazy.nvim.git",
 		"--branch=stable", -- latest stable release
