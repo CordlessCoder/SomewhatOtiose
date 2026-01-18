4dce59796a37e1e7548d39faf1715e5c5f66027e
CordlessCoder <roman@vm.net.ua>
2026-01-18 18:18:56 +0000

    Fix ty config

--- lsp/ty.lua
+++ lsp/ty.lua
@@ -1,9 +1,14 @@
 return {
 	filetypes = { "python" },
-	cmd = { "ty" },
+	cmd = { "ty", "server" },
 	settings = {
 		ty = {
 			-- ty language server settings go here
 		},
+	},
+	root_markers = {
+		".git",
+		"pyproject.toml",
+		"ty.toml",
 	},
 }
