3288736829a6af8299d03974f987172609097c47
CordlessCoder <roman@vm.net.ua>
2026-01-19 07:52:46 +0000

    Remove luasnip

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -23,12 +23,11 @@ return {
 
 		cmp.setup({
 			enabled = function()
-				return vim.g.cmp_enabled;
+				return vim.g.cmp_enabled
 			end,
 			sources = {
 				{ name = "nvim_lsp" },
 				{ name = "nvim_lua" },
-				{ name = "luasnip" },
 				{ name = "crates" },
 				{ name = "neorg" },
 				{ name = "path" },
@@ -62,11 +61,6 @@ return {
 				["<Tab>"] = cmp.mapping(function(fallback)
 					if cmp.visible() then
 						cmp.select_next_item()
-					elseif require("luasnip").expand_or_jumpable() then
-						vim.fn.feedkeys(
-							vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true),
-							""
-						)
 					else
 						fallback()
 					end
@@ -77,8 +71,6 @@ return {
 				["<S-Tab>"] = cmp.mapping(function(fallback)
 					if cmp.visible() then
 						cmp.select_prev_item()
-					elseif require("luasnip").jumpable(-1) then
-						vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-jump-prev", true, true, true), "")
 					else
 						fallback()
 					end
@@ -88,34 +80,6 @@ return {
 				}),
 			},
 		})
-
-		-- local cmp_window = require("cmp.utils.window")
-		--
-		-- cmp_window.info_ = cmp_window.info
-		-- cmp_window.info = function(self)
-		-- 	local info = self:info_()
-		-- 	info.scrollable = false
-		-- 	return info
-		-- end
-		--
-		-- local options = {
-		-- 	window = {
-		-- 		completion = {
-		-- 			border = border("CmpBorder"),
-		-- 			winhighlight = "Normal:CmpPmenu,CursorLine:PmenuSel,Search:None",
-		-- 		},
-		-- 		documentation = {
-		-- 			border = border("CmpDocBorder"),
-		-- 		},
-		-- 	},
-		-- 	snippet = {
-		-- 		expand = function(args)
-		-- 			require("luasnip").lsp_expand(args.body)
-		-- 		end,
-		-- 	},
-		-- }
-		--
-		-- cmp.setup(options)
 	end,
 	-- these dependencies will only be loaded when cmp loads
 	-- dependencies are always lazy-loaded unless specified otherwise
@@ -124,6 +88,5 @@ return {
 		"hrsh7th/cmp-buffer",
 		"hrsh7th/cmp-path",
 		"hrsh7th/cmp-cmdline",
-		"L3MON4D3/LuaSnip",
 	},
 }
